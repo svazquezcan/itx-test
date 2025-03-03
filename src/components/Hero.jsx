67e80a96b52d212459bde40550b3265eaaccd1de
Sandra Vázquez <svazquezcan@uoc.edu>
2025-03-03 18:51:08 +0100

    fix header

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import model from "../img/model.png";
 export const Hero = () => {
   return (
     <>
-      <section className="h-[800px] bg-hero bg-no-repeat bg-center bg-cover py-24 mt-20">
+      <section className="h-[800px] bg-hero bg-no-repeat bg-center bg-cover py-24">
         <div className="container mx-auto flex justify-around h-full">
           {/* texto */}
           <div className="flex flex-col justify-center">
